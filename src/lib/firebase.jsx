573eefe68a4cab8b5dd3ea9c4dca6f94719c8d33
Gonzalo <gonzalotorresgrau@gmail.com>
2024-07-12 12:29:49 -0300

    firebase

--- src/lib/firebase.jsx
+++ src/lib/firebase.jsx
@@ -1,38 +1,44 @@
 
-// import dotenv from 'dotenv';
-// dotenv.config();
-// import { initializeApp } from "firebase/app";
-// import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut, updateProfile,signInWithPopup  } from 'firebase/auth';
-// const { NEXT_API_KEY } = process.env
+import dotenv from 'dotenv';
+dotenv.config();
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+const { NEXT_API_KEY } = process.env
 
 
-// console.log(process.env.NEXT_API_KEY)
-// console.log(NEXT_API_KEY)
+console.log(process.env.NEXT_API_KEY)
+console.log(NEXT_API_KEY)
 
-// const firebaseConfig = {
-//     apiKey: process.env.NEXT_API_KEY,
-//     authDomain: process.env.NEXT_AUTH_DOMAIN,
-//     projectId: process.env.NEXT_PROJECT_ID,
-//     storageBucket: process.env.NEXT_STORAGE_BUCKET,
-//     messagingSenderId: process.env.NEXT_MESSAGING_SENDER_ID,
-//     appId: process.env.NEXT_APP_ID,
-//     measurementId: process.env.NEXT_MEASUREMENT_ID
-//   };
+const firebaseConfig = {
+    apiKey: process.env.NEXT_API_KEY,
+    authDomain: process.env.NEXT_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PROJECT_ID,
+    storageBucket: process.env.NEXT_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_APP_ID,
+    measurementId: process.env.NEXT_MEASUREMENT_ID
+  };
 
-// const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-// export default app;
+export default app;
 
-// export const auth = getAuth(app);
+export const auth = getAuth(app);
 
 
-// //------------------ Auth--------------------///
+//------------------ Auth--------------------///
 
-// // Función para iniciar sesión con usuario y contraseña
-// export const signIn = async(data)=>{
-//     const {email, contraseña} = data
-//     return await signInWithEmailAndPassword(auth, email, contraseña);
-// }
+// Función para iniciar sesión con usuario y contraseña
+export const signIn = async(data)=>{
+    const {email, contraseña} = data
+    return await signInWithEmailAndPassword(auth, email, contraseña);
+}
+
+// Función para cerrar sesión
+export const logOut = () =>{
+    signOut(auth);
+    return {message:'Cerro la sesion exitosamente'};
+}
 
 // // Función para crear usuario
 // export const createUser = async(data) =>{
@@ -45,11 +51,6 @@
 //     if(auth.currentUser) return updateProfile(auth.currentUser, user)
 // }
 
-// // Función para cerrar sesión
-// export const logOut = () =>{
-//     signOut(auth);
-//     return {message:'Cerro la sesion exitosamente'};
-// }
 
 // // Función para iniciar sesión con Google
 // export const singnInWithGoogle = async () => {
