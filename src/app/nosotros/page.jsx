ebfd98a3ab9ebea06b1016518a77b9cdae495278
Gonzalo <gonzalotorresgrau@gmail.com>
2025-01-27 15:16:20 -0300

    suspense

--- src/app/nosotros/page.jsx
+++ src/app/nosotros/page.jsx
@@ -1,5 +1,6 @@
 import BotonWsp from '@/components/BotonWSP/BotonWsp'
 import Footer from '@/components/Footer/Footer'
+import Loading from '@/components/Loading/Loading'
 import NavBar from '@/components/NavBar/NavBar'
 import NosotrosPage from '@/components/SobreMi/NosotrosPage'
 import VolverArriba from '@/components/VolverArriba/VolverArriba'
@@ -9,7 +10,9 @@ const NosPage = () => {
   return (
     <div className="flex flex-col h-screen">
       <nav className="">
-        <NavBar />
+        <Suspense fallback={<Loading/>}>
+            <NavBar />
+        </Suspense>
       </nav>
       <main className="flex-1 flex items-center justify-center bg-white">
         <NosotrosPage />
